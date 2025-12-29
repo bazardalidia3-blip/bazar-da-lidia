30a39d5f5a8b3bbe23b56e4197591d642f8b1b80
bazardalidia3-blip <bazardalidia3@gmail.com>
2025-12-29 10:05:44 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BazarDaLidia from "./BazarDaLidia";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BazarDaLidia />
+  </React.StrictMode>
+);
