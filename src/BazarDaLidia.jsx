d4259652193b81884ee7908f86a01ed941694652
bazardalidia3-blip <bazardalidia3@gmail.com>
2025-12-29 10:43:13 -0300

    Update BazarDaLidia.jsx

--- src/BazarDaLidia.jsx
+++ src/BazarDaLidia.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function BazarDaLidia() {
   const whatsapps = ["41995503761", "41984188454"];
